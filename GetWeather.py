48e57823a149a6967963dd82b75c428c96130054
sorrownight <34826771+sorrownight@users.noreply.github.com>
2020-03-08 21:36:34 -0700

    --Moved the update for DateTime and Calender/Weather to different threads to minimize queries of third-party sites --Moved DateTime to a different file and panel --Some documentation updated

--- GetWeather.py
+++ GetWeather.py
@@ -1,18 +1,21 @@
 
 from datetime import datetime
 import requests
+
+"""
+    :author Luan Ta
+    Query the weather from a JSON file of a third party site
+    Source: https://www.metaweather.com    
+"""
+
 detailed_weather = requests.get('https://www.metaweather.com/api/location/2490383/').json()  # Current Weather
 current_weather = detailed_weather['consolidated_weather'][0]
 
 
 def get_weather():
-    s = '[size=20][font=fonts/IndieFlower-Regular.ttf]Date: ' \
-        + str(datetime.now().month).zfill(2) + '/' + str(datetime.now().day).zfill(2) \
-        + '/' + str(datetime.now().year) + '[/font][/size]' + '\n' + get_time() \
-        + '\nThe Weather right now: ' + current_weather['weather_state_name'] + '\nTemperature: ' \
+    return '\nThe Weather right now: ' + current_weather['weather_state_name'] + '\nTemperature: ' \
         + format(celsius_to_fahrenheit(current_weather['the_temp']), '.0F') + ' F\nWind Speed: ' \
         + format(current_weather['wind_speed'], '.2F') + '\nHumidity: ' + str(current_weather['humidity'])
-    return s
 
 
 def celsius_to_fahrenheit(degree):
@@ -30,5 +33,3 @@ def get_weather_img():
     return 'img/' + status + '.png'
 
 
-def get_time():
-    return 'Time: ' + str(datetime.now().hour) + ':' + str(datetime.now().minute).zfill(2)
--- Main.py
+++ Main.py
@@ -7,23 +7,17 @@ from gui.widget.Widget import Widget
 from kivy.config import Config
 import GetCalendar
 import GetWeather
+import GetDateTime
 
 """
     :author Luan Ta
-    :version initial - 0.0.2 - 01/30/2020
+    :version beta - 1.1.0 - 03/08/2020
     Main class of the program. Executes the GUI display.
-
-    :TODO
-        Change background color- DONE
-        Transitioning text using Queue - IN PROGRESS
-        Support for image - DONE
-        Support for touchscreen-interaction
-        Linux testing - DONE
-        MORE FEATURE-CREEPS
 """
 WINDOW_WIDTH = 1080
 WINDOW_HEIGHT = 720
-UPDATE_INTERVAL = 60  # in seconds
+UPDATE_INTERVAL_WEATHER_CALENDER = 3600  # in seconds
+UPDATE_INTERVAL_TIME = 1  # in seconds
 
 
 class Main(App):
@@ -33,15 +27,20 @@ class Main(App):
         Config.set('graphics', 'width', str(WINDOW_WIDTH))
         Config.set('graphics', 'height', str(WINDOW_HEIGHT))
         self.layout = BoxLayout(orientation='vertical', size=(WINDOW_WIDTH, WINDOW_HEIGHT))
-        self.widget = Widget(self.layout, 2)
+        self.widget = Widget(self.layout, 3)
         self.widget.change_background(0, 0.3, 0.5, 1)
-        self.update()
-        Clock.schedule_interval(self.update, UPDATE_INTERVAL)
+        self.update_calender_weather()
+        self.update_time()
+        Clock.schedule_interval(self.update_time, UPDATE_INTERVAL_TIME)
+        Clock.schedule_interval(self.update_calender_weather, UPDATE_INTERVAL_WEATHER_CALENDER)
 
-    def update(self, *args):
-        self.widget.add_image(GetWeather.get_weather_img(), 250, 600, 100, 100)
-        self.widget.change_panel_text(GetWeather.get_weather(), 1)
-        self.widget.change_panel_text(GetCalendar.get_events(1), 2)
+    def update_calender_weather(self, *args):
+        self.widget.add_image(GetWeather.get_weather_img(), 300, 300, 100, 100)
+        self.widget.change_panel_text(GetWeather.get_weather(), 2)
+        self.widget.change_panel_text(GetCalendar.get_events(5), 3)
+
+    def update_time(self, *args):
+        self.widget.change_panel_text(GetDateTime.get_time(), 1)
 
     def build(self):
         return self.layout
